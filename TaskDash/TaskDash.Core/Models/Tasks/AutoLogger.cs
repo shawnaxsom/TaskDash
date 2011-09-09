609e3b9feb61f01c4dda1fd6e3a9a0b539f4ef82
Shawn Axsom <axs221@gmail.com>
2011-09-09 02:30:42 -0400

    minor cleanup

---
+++ TaskDash/TaskDash.Core/Models/Tasks/AutoLogger.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using Norm.Attributes;
+using TaskDash.Core.Utilities;
+
+namespace TaskDash.Core.Models.Tasks
+{
+    public class AutoLogger : ModelBase<AutoLogger>
+    {
+        private readonly TaskStopwatch _stopWatch;
+        private TimeSpan _timeBetweenPrompts;
+
+        public AutoLogger()
+        {
+            _stopWatch = new TaskStopwatch();
+            _stopWatch.Tick += OnStopWatchTick;
+        }
+
+        private void OnStopWatchTick(object sender, EventArgs e)
+        {
+            LoggingRequested(this, new LoggingRequestedEventHandlerArgs());
+        }
+
+        public event LoggingRequestedEventHandler LoggingRequested;
+
+        [MongoIgnore]
+        public string TimeBetweenPrompts
+        {
+            get { return _timeBetweenPrompts.ToString(@"hh\:mm\:ss"); }
+            set
+            {
+                TimeSpan newTime;
+
+                if (TimeSpan.TryParse(value, out newTime))
+                {
+                    _timeBetweenPrompts = newTime;
+                    OnPropertyChanged("TimeBetweenPrompts");
+                }
+            }
+        }
+
+        [MongoIgnore]
+        public override string EditableValue
+        {
+            get { return TimeBetweenPrompts; }
+            set { TimeBetweenPrompts = value; }
+        }
+
+        public void Start()
+        {
+            _stopWatch.Start();
+        }
+    }
+
+    public delegate void LoggingRequestedEventHandler(object sender, LoggingRequestedEventHandlerArgs args);
+
+    public class LoggingRequestedEventHandlerArgs : EventArgs
+    {
+        public LoggingRequestedEventHandlerArgs()
+        {
+        }
+    }
+}
---
+++ TaskDash/TaskDash/MainWindowViewModel.cs
@@ -0,0 +1,83 @@
+﻿using System.Windows;
+using System.Windows.Data;
+using TaskDash.Core;
+using TaskDash.Core.Models.Tasks;
+using TaskDash.ViewModels;
+
+namespace TaskDash
+{
+    /// <summary>
+    /// http://en.wikipedia.org/wiki/Model_View_ViewModel
+    /// </summary>
+    public class MainWindowViewModel : ViewModelBase<MainWindowViewModel>
+    {
+        public MainWindowViewModel()
+        {
+            Tasks = new Task().GetTasks();
+            FilteredTasks = new CollectionViewSource { Source = this.Tasks };
+
+            _autoLogger = new AutoLogger()
+                              {
+                                  TimeBetweenPrompts = "00:00:10"
+                              };
+            _autoLogger.LoggingRequested += new LoggingRequestedEventHandler(_autoLogger_LoggingRequested);
+            _autoLogger.Start();
+        }
+
+        private void _autoLogger_LoggingRequested(object sender, LoggingRequestedEventHandlerArgs args)
+        {
+            //TODO: CurrentTask is always null
+            if (CurrentTask == null) return;
+
+            LoggingRequestDialogViewModel viewModel = new LoggingRequestDialogViewModel(CurrentTask.Logs);
+            LoggingRequestDialog dialog = new LoggingRequestDialog(viewModel);
+            dialog.Show();
+        }
+
+        //private Task _currentTask;
+        //public Task CurrentTask
+        //{
+        //    get { return _currentTask; }
+        //    set
+        //    {
+        //        _currentTask = value;
+        //        OnPropertyChanged("CurrentTask");
+        //        OnPropertyChanged("CurrentTask.Key");
+        //        OnPropertyChanged("Key");
+        //    }
+        //}
+        private static readonly DependencyProperty CurrentTaskProperty =
+           DependencyProperty.Register("CurrentTask", typeof(Task), typeof(MainWindowViewModel),
+                                       new PropertyMetadata(null));
+        public Task CurrentTask
+        {
+            get { return (Task) GetValue(CurrentTaskProperty); }
+            set { SetValue(CurrentTaskProperty, value); }
+        }
+
+        public CollectionViewSource FilteredTasks { get; set; }
+        private Tasks _tasks;
+        private AutoLogger _autoLogger;
+
+        public Tasks Tasks
+        {
+            get { return _tasks; }
+            private set
+            {
+                _tasks = value;
+                i = 0;
+            }
+        }
+
+        private void AddNewTask()
+        {
+            Tasks.Add(new Task() { _Id = i.ToString() });
+            i++;
+        }
+
+        public void Save()
+        {
+            Tasks.Save();
+        }
+    }
+}
