a8a47742dc2bfb1cf399b03556d3e7bd478b5119
Shawn Axsom <axs221@gmail.com>
2011-09-08 23:05:32 -0400

    Added LoggingRequestDialog. It is currently incomplete--passes in a null reference.

---
+++ TaskDash/TaskDash.Core/ExtensionMethods/Bools.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+
+namespace TaskDash.Core.ExtensionMethods
+{
+    public static class Bools
+    {
+        public static Visibility ToVisible(this bool setVisible)
+        {
+            if (setVisible)
+            {
+                return Visibility.Visible;
+            }
+            else
+            {
+                return Visibility.Collapsed;
+            }
+        }
+
+        public static Visibility Toggle(this Visibility current)
+        {
+            if (current == Visibility.Visible)
+            {
+                return Visibility.Collapsed;
+            }
+            else
+            {
+                return Visibility.Visible;
+            }
+        }
+    }
+}
---
+++ TaskDash/TaskDash/LoggingRequestDialog.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TaskDash
+{
+    /// <summary>
+    /// Interaction logic for LoggingRequestDialog.xaml
+    /// </summary>
+    public partial class LoggingRequestDialog : Window
+    {
+        private readonly LoggingRequestDialogViewModel _viewModel;
+
+        public LoggingRequestDialog(LoggingRequestDialogViewModel viewModel)
+        {
+            _viewModel = viewModel;
+
+            InitializeComponent();
+        }
+    }
+}
