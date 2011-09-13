123ff8e4d4cb92881a0cb634befa0fcca6b17d92
Shawn Axsom <axs221@gmail.com>
2011-09-13 17:58:07 -0400

    Included a few recent new files

---
+++ TaskDash/TaskDash.Controls/Dialogs/EditTaskItem.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Input;
+using TaskDash.Core.Models.Tasks;
+
+namespace TaskDash.CustomControls.Dialogs
+{
+    /// <summary>
+    /// Interaction logic for TaskItem.xaml
+    /// </summary>
+    public partial class EditTaskItem : Window
+    {
+        private readonly ICollectionView _view;
+        private TaskItem _taskItem;
+
+        public EditTaskItem(ICollectionView view, TaskItem taskItem)
+        {
+            InitializeComponent();
+
+            this.DataContext = taskItem;
+            _view = view;
+            _taskItem = taskItem;
+
+            this.Loaded += OnEditTaskItemLoaded;
+            this.Closed += OnClosed;
+        }
+
+        private void OnClosed(object sender, EventArgs e)
+        {
+            _view.Refresh();
+        }
+
+        void OnEditTaskItemLoaded(object sender, RoutedEventArgs e)
+        {
+            textBoxNotes.Focus();
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (Keyboard.IsKeyDown(Key.LeftCtrl) || Keyboard.IsKeyDown(Key.RightCtrl))
+            {
+                if (e.Key == Key.W)
+                {
+                    this.Close();
+                }
+            }
+            else
+            {
+                if (e.Key == Key.Escape)
+                {
+                    this.Close();
+                }
+            }
+        }
+    }
+}
---
+++ TaskDash/TaskDash.UserControls/Tasks/UserControlViewBase.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Windows.Controls;
+using System.Windows.Input;
+using TaskDash.CustomControls;
+
+namespace TaskDash.UserControls
+{
+    public class UserControlViewBase : UserControl
+    {
+        /// <summary>
+        /// Checks if focused control is a text box.
+        /// This includes text boxes in user controls.
+        /// </summary>
+        public bool IsEditing
+        {
+            get
+            {
+                Type type = Keyboard.FocusedElement.GetType();
+
+                return type == typeof(TextBox);
+            }
+        }
+
+        protected void AddItemOrFocus(ListBoxWithAddRemove listBox)
+        {
+            //if (!IsEditing)
+            //{
+            if (Keyboard.IsKeyDown(Key.LeftShift)
+                || Keyboard.IsKeyDown(Key.RightShift))
+            {
+                listBox.Focus();
+            }
+            else
+            {
+                listBox.SimulateClick(listBox.AddButton);
+            }
+            //}
+        }
+    }
+}
